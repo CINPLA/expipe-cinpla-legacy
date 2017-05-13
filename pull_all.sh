d2871809383b5f5d54f4be7088dc4a1edb0f9c94
Mikkel E Lepperød <lep.mik@gmail.com>
2017-05-13 22:19:36 +0200

    subrepo fix

--- pull_all.sh
+++ pull_all.sh
@@ -1,7 +1,5 @@
 for D in python-neo elephant exdir expipe expipe-browser expipe-cli exdir-cli expipe-plugin-neuro exdir-browser expipe-io-neuro exana pyxona; do
     if [ -d "$D" ]; then
-        cd "$D"
-        git subrepo pull
-        cd ..
+        git subrepo pull "$D"
     fi
 done
