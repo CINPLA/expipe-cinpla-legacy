ee9142b575979c1fbdb6628fe6f3aa5004414e87
Mikkel E Lepperød <lep.mik@gmail.com>
2017-05-13 22:34:39 +0200

    add phy

--- setup_all_no_browser.sh
+++
@@ -1,7 +0,0 @@
-for D in python-neo elephant exdir expipe expipe-browser expipe-cli exdir-cli expipe-plugin-cinpla exdir-browser expipe-io-neuro exana pyxona; do
-    if [ -d "$D" ]; then
-        cd "$D"
-        python setup.py develop
-        cd ..
-    fi
-done
