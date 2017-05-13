ee1f580610247641e8e501054a4b8b298ff4aa6c
Mikkel E Lepperød <lep.mik@gmail.com>
2017-05-13 23:09:01 +0200

    temp remove intan

--- pull_all.sh
+++
@@ -1,5 +0,0 @@
-for D in python-neo elephant exdir expipe expipe-browser expipe-cli exdir-cli expipe-plugin-cinpla exdir-browser expipe-io-neuro exana pyxona phy phy-contrib; do
-    if [ -d "$D" ]; then
-        git subrepo pull "$D"
-    fi
-done
