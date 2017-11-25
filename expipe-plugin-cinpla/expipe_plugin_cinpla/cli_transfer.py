b2f8b7103ae59267e946ef5a45724d51f3c85f1c
Espen Hagen <e.hagen@fz-juelich.de>
2017-11-25 15:22:10 +0100

    replaces --override-prompts flag with --yes

--- expipe-plugin-cinpla/expipe_plugin_cinpla/cli_transfer.py
+++ expipe-plugin-cinpla/expipe_plugin_cinpla/cli_transfer.py
@@ -63,14 +63,14 @@ def attach_to_cli(cli):
                   type=click.STRING,
                   help='Name of server as named in config.yaml. Default is "nird"',
                   )
-    @click.option('--override-prompts',
+    @click.option('-y', '--yes',
                   is_flag=True,
                   default=False,
                   help='disables yes/no prompts for automated removal of files after transfer. Default is False')
     def transfer(action_id, to_local, from_local, overwrite, no_trash,
                  exclude, include, port, username,
                  hostname, recursive, server, move,
-                 override_prompts):
+                 yes):
         assert server in expipe.config.settings
         server_dict = expipe.config.settings.get(server)
         if len(exclude) > 0 and len(include) > 0:
@@ -162,7 +162,7 @@ def attach_to_cli(cli):
             if not no_trash and not move:
                 try:
                     from send2trash import send2trash
-                    if override_prompts:
+                    if yes:
                         print('thrashing local data "' + local_data + '"')
                         send2trash(local_data)
                     else:
@@ -174,7 +174,7 @@ def attach_to_cli(cli):
                 except Exception:
                     warnings.warn('Unable to send local data to trash')
             if move:
-                if override_prompts:
+                if yes:
                     print('Deleting "' + local_data + '".')
                     shutil.rmtree(local_data)
                 else:
