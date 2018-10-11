49f1024db03a6f97d2530bbebd21614929dfaca9
Mikkel Elle Lepperød <lep.mik@gmail.com>
2018-10-11 08:15:16 +0200

    git subrepo pull py-open-ephys
    subrepo:
      subdir:   "py-open-ephys"
      merged:   "2ffeccb"
    upstream:
      origin:   "git@github.com:CINPLA/py-open-ephys.git"
      branch:   "master"
      commit:   "2ffeccb"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a7ee886"

--- py-open-ephys/setup.py
+++ py-open-ephys/setup.py
@@ -1,18 +1,31 @@
-# -*- coding: utf-8 -*-
-from setuptools import setup
-import os
-
 from setuptools import setup, find_packages
+# import pyopenephys
 
 long_description = open("README.md").read()
 
 entry_points = None
 
-install_requires = []
-
 setup(
     name="pyopenephys",
+    version='0.1',
+    author="Alessio Buccino",
+    author_email="alessiob@ifi.uio.no",
+    description="Python package for parsing Open Ephys data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CINPLA/py-open-ephys",
     packages=find_packages(),
     entry_points=entry_points,
     include_package_data=True,
+    install_requires=[
+        'numpy',
+        'scipy',
+        'quantities',
+        'xmltodict',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ]
 )
