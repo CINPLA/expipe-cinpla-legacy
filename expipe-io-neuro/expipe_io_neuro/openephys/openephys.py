3dbec6431cdce9394cfbe554bc83acea2ab894a7
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2018-10-24 13:02:24 +0200

    Rectified MUA signal calculation etc. (#8)
    Rectified MUA signal calculation etc. needed for COBRA_visual_ephys module

--- expipe-io-neuro/expipe_io_neuro/openephys/openephys.py
+++ expipe-io-neuro/expipe_io_neuro/openephys/openephys.py
@@ -4,6 +4,9 @@ import glob
 import os
 import quantities as pq
 import numpy as np
+import scipy.signal as ss
+import copy
+
 
 # from expipe.core import Filerecord
 # from expipe.core import user
@@ -60,8 +63,6 @@ def _prepare_channel_groups(exdir_path, openephys_rec):
 
 
 def generate_lfp(exdir_path, openephys_rec):
-    import scipy.signal as ss
-    import copy
     exdir_channel_groups = _prepare_channel_groups(exdir_path, openephys_rec)
     for channel_group, openephys_channel_group in zip(exdir_channel_groups,
                                                       openephys_rec.channel_groups):
@@ -95,6 +96,74 @@ def generate_lfp(exdir_path, openephys_rec):
                 data.attrs["num_samples"] = len(signal)
                 # NOTE: In exdirio (python-neo) sample rate is required on dset #TODO
                 data.attrs["sample_rate"] = sample_rate
+
+
+def generate_mua(exdir_path, openephys_rec, N=2, fcrit=300.*pq.Hz, car=True):
+    '''
+    Parameters
+    ----------
+    exdir_path : path
+        path to exdir directory
+    openephys_rec :
+        pyopenephys.core.Recording object
+    N : int
+        Butterworth filter order. Default is 2
+    fcrit : float*pq.Hz
+        Critical frequency for butterworth highpass filter
+    car : bool
+        subtract the mean non-rectified mua from the signals. Default is True
+    '''
+    exdir_channel_groups = _prepare_channel_groups(exdir_path, openephys_rec)
+    for channel_group, openephys_channel_group in zip(exdir_channel_groups,
+                                                      openephys_rec.channel_groups):
+        mua = channel_group.require_group("MUA")
+        group_id = openephys_channel_group.id
+        print('Generating MUA, channel group ', group_id)
+        if car:
+            for i, channel in enumerate(openephys_channel_group.channels):
+                analog_signal = openephys_channel_group.analog_signals[channel.index]
+                sample_rate = copy.copy(openephys_rec.sample_rate)
+                b, a = ss.butter(N=N, Wn=(fcrit/sample_rate/2), btype='high')
+                if i == 0:
+                    mean = ss.filtfilt(b, a, np.array(analog_signal.signal, dtype=float), axis=-1)
+                else:
+                    mean += ss.filtfilt(b, a, np.array(analog_signal.signal, dtype=float), axis=-1)
+
+            mean /= len(openephys_channel_group.analog_signals)
+
+        for channel in openephys_channel_group.channels:
+            mua_timeseries = mua.require_group(
+                "MUA_timeseries_{}".format(channel.index)
+            )
+            analog_signal = openephys_channel_group.analog_signals[channel.index]
+            # highpass-filter data
+            target_rate = 1000 * pq.Hz
+            signal = np.array(analog_signal.signal, dtype=float)
+            sample_rate = copy.copy(openephys_rec.sample_rate)
+            b, a = ss.butter(N=N, Wn=(fcrit/sample_rate/2), btype='high')
+            signal = ss.filtfilt(b, a, signal, axis=-1)
+            if car:
+                signal -= mean
+            # rectify
+            signal = abs(signal)
+            # decimate
+            q = int(openephys_rec.sample_rate / target_rate)
+            signal = ss.decimate(signal, q=q, n=q*2, ftype='fir', zero_phase=True)
+            sample_rate /= q
+            t_stop = len(signal) / sample_rate
+            assert round(t_stop, 1) == round(openephys_rec.duration, 1), '{}, {}'.format(t_stop, openephys_rec.duration)
+            signal = signal * channel.gain
+            mua_timeseries.attrs["num_samples"] = len(signal)
+            mua_timeseries.attrs["start_time"] = 0 * pq.s
+            mua_timeseries.attrs["stop_time"] = t_stop
+            mua_timeseries.attrs["sample_rate"] = sample_rate
+            mua_timeseries.attrs["electrode_identity"] = analog_signal.channel_id
+            mua_timeseries.attrs["electrode_idx"] = analog_signal.channel_id - openephys_channel_group.id * 4
+            mua_timeseries.attrs['electrode_group_id'] = group_id
+            data = mua_timeseries.require_dataset("data", data=signal)
+            data.attrs["num_samples"] = len(signal)
+            # NOTE: In exdirio (python-neo) sample rate is required on dset #TODO
+            data.attrs["sample_rate"] = sample_rate
 
 
 def generate_spike_trains(exdir_path, openephys_rec, source='klusta'):
@@ -152,6 +221,57 @@ def generate_spike_trains(exdir_path, openephys_rec, source='klusta'):
                 chx.units.append(unit)
             exdirio.write_channelindex(chx, start_time=0 * pq.s,
                                        stop_time=openephys_rec.duration)
+    elif source == 'kilosort':
+        print('Generating spike trains from KiloSort')
+        exdirio = neo.io.ExdirIO(exdir_path)
+        exdir_file = exdir.File(exdir_path)
+        openephys_directory = op.join(str(exdir_file["acquisition"].directory),
+                                      exdir_file["acquisition"].attrs["openephys_session"])
+        # iterate over channel groups. As there are no channel associated with
+        # any spiking unit (TO BE IMPLEMENTED), everything is written to
+        # channel_group 0
+        for oe_group in openephys_rec.channel_groups:
+            channel_ids = [ch.id for ch in oe_group.channels]
+            channel_index = [ch.index for ch in oe_group.channels]
+            chx = neo.ChannelIndex(
+                name='channel group {}'.format(oe_group.id),
+                channel_ids=channel_ids,
+                index=channel_index,
+                group_id=oe_group.id
+            )
+            # load output
+            spt = np.load(op.join(openephys_directory,
+                                  'spike_times.npy')).flatten()
+            spc = np.load(op.join(openephys_directory,
+                                  'spike_clusters.npy')).flatten()
+            try:
+                cgroup = np.loadtxt(op.join(openephys_directory,
+                                            'cluster_group.tsv'),
+                                          dtype=[('cluster_id', 'i4'), ('group', 'U8')],
+                                          skiprows=1)
+                if cgroup.shape == (0, ):
+                    raise FileNotFoundError
+            except FileNotFoundError:
+                # manual corrections didn't happen;
+                cgroup = np.array(list(zip(np.unique(spc),
+                                           ['unsorted']*np.unique(spc).size)),
+                                  dtype=[('cluster_id', 'i4'), ('group', 'U8')])
+            for id, grp in cgroup:
+                unit = neo.Unit(
+                    cluster_group = str(grp),
+                    cluster_id = id,
+                    name = id
+                )
+                unit.spiketrains.append(
+                    neo.SpikeTrain(
+                        times = (spt[spc==id].astype(float) / openephys_rec.sample_rate).simplified,
+                        t_stop = openephys_rec.duration,
+                    )
+                )
+                chx.units.append(unit)
+            exdirio.write_channelindex(chx, start_time=0 * pq.s,
+                                       stop_time=openephys_rec.duration)
+            break
     else:
         raise ValueError(source + ' not supported')
 
